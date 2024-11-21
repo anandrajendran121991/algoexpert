b1d95fc40e0a16856bfc3619de94b079b478c036
anandrajendran121991 <anand.rajendran121991@gmail.com>
2024-11-21 17:34:19 +0800

    Is palindrome

---
+++ easy/is_palindrome.js
@@ -0,0 +1,23 @@
+/**
+ * @description Solution using two pointers 
+ * @param {string[]} string 
+ * @returns {boolean}
+ * @complexities Time => O(n) | Space => O(1)
+ */
+const isPalindrome = (string) => {
+  let i = 0;
+  let j = string.length - 1;
+  let isPalindromeString = true;
+  while (i < j) {
+    if (string[i] === string[j]) {
+      i++;
+      j--;
+    } else {
+      isPalindromeString = false;
+      return isPalindromeString;
+    }
+  }
+  return isPalindromeString;
+};
+
+console.log(isPalindrome("abcdvgcba"));
